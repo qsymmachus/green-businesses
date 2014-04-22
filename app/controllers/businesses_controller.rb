8e781153294071ac86c8046ca1bf184506ca1d22
John Olmsted <johnaolmsted@gmail.com>
2014-04-22 11:15:18 -0700

    Search is now performed asynchronously through an AJAX request

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -5,8 +5,8 @@ end
 post '/search' do
   @results = Business.search_company(params[:company])
   if @results
-    erb :search_results
+    erb :search_results, layout: false
   else
-    erb :no_results
+    erb :no_results, layout: false
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  Search.bindEvents()
 });
+
+var Search = {
+  bindEvents: function() {
+    $('#search').on("submit", this.searchCompany)
+  },
+
+  searchCompany: function(e) {
+    e.preventDefault();
+    $('#results').html("<p id='status'>Searching...</p>")
+    searchTerm = $(this).serialize();
+    $.ajax ({
+      type: 'post',
+      url: '/search',
+      data: searchTerm
+    }).done(function(results) {
+      $('#results').html(results);
+    }).fail(function() {
+      $('#results').html("<p id='status'>We're sorry, but an error occurred.</p>");
+    })
+  }
+}
